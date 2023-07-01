98bbe4b1dd222ad70d64ceeabbac2e173ab1a816
aieckxis <aleckxiskate@gmail.com>
2023-07-01 19:07:40 +0800

    methods

--- car.py
+++ car.py
@@ -8,6 +8,17 @@ class Car:
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
-# Method to accelerate the car's speed by 5
-# Method to brake and decrease the car's speed by 5 units
-# Method to get the current speed of the car
+
+    # Method to accelerate the car's speed by 5
+    def accelerate(self):
+        # Increase the car's speed by 5
+        self.__speed += 5
+
+    # Method to brake and decrease the car's speed by 5 units
+    def brake(self):
+        # Decrease the car's speed by 5
+        self.__speed -= 5
+
+    # Method to get the current speed of the car
+    def get_speed(self):
+        return self.__speed
