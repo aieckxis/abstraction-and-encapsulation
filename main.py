de0718179c03a7ec90bf85de21d7c910deedd37c
aieckxis <aleckxiskate@gmail.com>
2023-07-01 21:38:16 +0800

    text color

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ from car import Car
 def main():
     car = Car(2003, "Mercedes-Benz")
     # Print the initial speed of the car
-    print("Initial speed: ", car.get_speed())
+    print("\033[1mInitial speed: ", car.get_speed(), "\033[0m")
+    print("")
 
     # Accelerate the car five times
     for _ in range(5):
         car.accelerate()
-        print("Current speed after accelerating:", car.get_speed())
+        print("\033[32mCurrent speed after accelerating:", car.get_speed(), "\033[0m")
 
+    print("")
     # Brake the car five times
     for _ in range(5):
         car.brake()
-        print("Current speed after braking:", car.get_speed())
+        print("\033[31mCurrent speed after braking:", car.get_speed(), "\033[0m")
 
 # Check if the current file is the main file being executed
 if __name__ == "__main__":
