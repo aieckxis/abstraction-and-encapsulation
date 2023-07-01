75a540c34d92daa5a7d427839cb0e96978a5bfb4
aieckxis <aleckxiskate@gmail.com>
2023-07-01 15:03:53 +0800

    text color

--- test_fan.py
+++ test_fan.py
@@ -9,16 +9,19 @@ class TestFan:
         fan2 = Fan(Fan.MEDIUM, 5, 'blue', False)
 
         # Display properties of the first fan
-        print("Fan 1 - Speed:", fan1.get_speed())
-        print("Fan 1 - Radius:", fan1.get_radius())
-        print("Fan 1 - Color:", fan1.get_color())
-        print("Fan 1 - On:", fan1.is_on())
+        print("\033[1mFAN 1\033[0m")
+        print("\033[93mFan 1 - Speed:", fan1.get_speed(), "\033[0m")
+        print("\033[93mFFan 1 - Radius:", fan1.get_radius(), "\033[0m")
+        print("\033[93mFFan 1 - Color:", fan1.get_color(), "\033[0m")
+        print("\033[93mFFan 1 - On:", fan1.is_on(), "\033[0m")
+        print("")
 
         # Display properties of the second fan
-        print("Fan 2 - Speed:", fan2.get_speed())
-        print("Fan 2 - Radius:", fan2.get_radius())
-        print("Fan 2 - Color:", fan2.get_color())
-        print("Fan 2 - On:", fan2.is_on())
+        print("\033[1mFAN 2\033[0m")
+        print("\033[94mFFan 2 - Speed:", fan2.get_speed(), "\033[0m")
+        print("\033[94mFan 2 - Radius:", fan2.get_radius(), "\033[0m")
+        print("\033[94mFan 2 - Color:", fan2.get_color(), "\033[0m")
+        print("\033[94mFan 2 - On:", fan2.is_on(), "\033[0m")
 
 # Create an instance  of the TestFan and run the test
 test = TestFan()
