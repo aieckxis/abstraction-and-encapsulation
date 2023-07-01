76ec36623a14383b862eba0f944ede198ee67282
aieckxis <aleckxiskate@gmail.com>
2023-07-01 18:47:08 +0800

    pseudocode for main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+# Import the Car from car.py
+# Main function to test the Car class
+# Print the initial speed of the car
+# Accelerate the car five times
+# Brake the car five times
+# Check if the current file is the main file being executed
