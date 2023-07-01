82e46ffad2bd62996e8e6229fa94e8e539845fbf
aieckxis <aleckxiskate@gmail.com>
2023-07-01 18:36:27 +0800

    pseudocode for car.py

---
+++ car.py
@@ -0,0 +1,8 @@
+# Bernabe, Aleckxis Kate V.
+# BSCpE 1-4
+
+# Car class represents a car object
+# Constructor to initialize the car's year model, make, and speed
+# Method to accelerate the car's speed by 5
+# Method to brake and decrease the car's speed by 5 units
+# Method to get the current speed of the car
