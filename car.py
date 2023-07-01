201123a7cf6b32b4c23ef518ef86420bda666775
aieckxis <aleckxiskate@gmail.com>
2023-07-01 18:52:41 +0800

    constructor to initialize the car's year model, make, and speed

--- car.py
+++ car.py
@@ -2,7 +2,12 @@
 # BSCpE 1-4
 
 # Car class represents a car object
-# Constructor to initialize the car's year model, make, and speed
+class Car:
+    # Constructor to initialize the car's year model, make, and speed
+    def __init__(self):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
 # Method to accelerate the car's speed by 5
 # Method to brake and decrease the car's speed by 5 units
 # Method to get the current speed of the car
