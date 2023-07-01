27beb50c864f23d1ca8d4cc85649e112f5ed266b
aieckxis <aleckxiskate@gmail.com>
2023-07-01 14:21:05 +0800

    created two fan objects

--- test_fan.py
+++ test_fan.py
@@ -4,7 +4,13 @@ from fan import Fan
 class TestFan:
     # Define class TestFan
     def run_test(self):
-# Create two Fan objects
-# Display properties of the first fan
-# Display properties of the second fan
-# Create an instance  of the Testfan and run the test
+        # Create two Fan objects
+        fan1 = Fan(Fan.FAST, 10, 'yellow', True)
+        fan2 = Fan(Fan.MEDIUM, 5, 'blue', False)
+
+        # Display properties of the first fan
+        # Display properties of the second fan
+
+# Create an instance  of the TestFan and run the test
+test = TestFan()
+test.run_test()
