38b2b28e226fa3fce46ae2ee1a29829623761363
aieckxis <aleckxiskate@gmail.com>
2023-07-01 13:07:28 +0800

    pseudocode for test_fan

---
+++ test_fan.py
@@ -0,0 +1,6 @@
+# Import Fan from fan
+# Define class TestFan
+# Create two Fan objects
+# Display properties of the first fan
+# Display properties of the second fan
+# Create an instance  of the Testfan and run the test
