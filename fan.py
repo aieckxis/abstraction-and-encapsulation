99d96f49d1de34aef6b56d3e9c1a5570fed2d2af
aieckxis <aleckxiskate@gmail.com>
2023-07-01 13:36:44 +0800

    constants for fan speed and constructor to initialize the fan object

--- fan.py
+++ fan.py
@@ -1,6 +1,17 @@
 # Bernabe, Aleckxis Kate V.
 # BSCpE 1-4
 
-# Constants for fan speed
-# Constructor to initialize the Fan object
+class Fan:
+    # Constants for fan speed
+    SLOW = 1
+    MEDIUM = 2
+    FAST = 3
+
+    def __init__(self, speed=1, radius=1, color='blue', on=False):
+        # Constructor to initialize the Fan object
+        self.speed = speed
+        self.radius = radius
+        self.color = color
+        self.on = 1
+
 # Getters and setters for all data fields
