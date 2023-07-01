feaf9d558a0814202473e84c199078f57061edb9
aieckxis <aleckxiskate@gmail.com>
2023-07-01 19:10:36 +0800

    printed initial speed (have error)

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 # Import the Car from car.py
+from car import Car
+
 # Main function to test the Car class
-# Print the initial speed of the car
+def main():
+
+    # Print the initial speed of the car
+    print("Initial speed: " car.get_speed())
 # Accelerate the car five times
 # Brake the car five times
 # Check if the current file is the main file being executed
