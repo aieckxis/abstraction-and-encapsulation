ef7812c72513c6c1640d4b0af9cac51d3d7d81a2
aieckxis <aleckxiskate@gmail.com>
2023-07-02 12:12:38 +0800

    pseudocode for pet.py

---
+++ pet.py
@@ -0,0 +1,11 @@
+# Bernabe,Aleckxis Kate V.
+# BSCpE 1-4
+
+# Pet class represents pet data
+# Initialize private data attributes for name, animal type, and age
+# Set the value of the name attribute
+# Set the value of the animal_type attribute
+# Set the value of the age attribute
+# Return the value of the name attribute
+# Return the value of the animal_type attribute
+# Return the value of the age attribute
