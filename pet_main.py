602a2cbf17389c47f6a5765973c95ee1dc62fe46
aieckxis <aleckxiskate@gmail.com>
2023-07-02 12:54:12 +0800

    asked the user and tried to print pet's name (have error)

--- pet_main.py
+++ pet_main.py
@@ -6,6 +6,16 @@ def main():
     # Create an instance for Pet class
     pet = Pet()
 
-# Ask the user for pet's information
-# Print the pet's information
+    # Ask the user for pet's information
+    name = input("Enter the name of your pet: ")
+    pet.set_name()
+
+    animal_type = input("Enter the type of animal your pet is: ")
+    pet.set_animal_type()
+
+    age = input("Enter the age of your pet: ")
+    pet.set_age()
+
+    # Print the pet's information
+    print("Pet's name: ", pet.get_name())
 # Check if the current file is the main file being executed
