88c27f3248576f64c3aa73b4d479a9c65c2efff9
aieckxis <aleckxiskate@gmail.com>
2023-07-02 12:20:05 +0800

    pseudocode for pet_main.py

---
+++ pet_main.py
@@ -0,0 +1,5 @@
+# Import Pet from pet
+# Create an instance for Pet class
+# Ask the user for pet's information
+# Print the pet's information
+# Check if the current file is the main file being executed
